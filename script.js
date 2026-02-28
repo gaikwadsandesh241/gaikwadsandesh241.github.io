5c655aec9b5c11f340c8b39f2a2de758b54b2799
gaikwadsandesh241 <gaikwadsandesh241@gmail.com>
2026-02-28 14:36:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 
 window.addEventListener("scroll", () => {
     const heroText = document.querySelectorAll(".hero-text");
+    const aboutSection = document.getElementById("about-section");
     const scrollY = window.scrollY;
 
     heroText.forEach(text => {
         if (scrollY > 150) {
             text.style.opacity = "0";
             text.style.transform = "translateY(-20px)";
+            aboutSection.classList.add("centered");
         } else {
             text.style.opacity = "1";
             text.style.transform = "translateY(0)";
+            aboutSection.classList.remove("centered");
         }
     });
 });
+
 
 
 
