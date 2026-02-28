9aadcd3aee07b1dccf91c83fef769c36da22114d
gaikwadsandesh241 <gaikwadsandesh241@gmail.com>
2026-02-28 13:58:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("Portfolio Loaded Successfully");
+
+
+
+window.addEventListener("scroll", () => {
+    const heroText = document.querySelectorAll(".hero-text");
+    const scrollY = window.scrollY;
+
+    heroText.forEach(text => {
+        if (scrollY > 150) {
+            text.style.opacity = "0";
+            text.style.transform = "translateY(-20px)";
+        } else {
+            text.style.opacity = "1";
+            text.style.transform = "translateY(0)";
+        }
+    });
+});
+
